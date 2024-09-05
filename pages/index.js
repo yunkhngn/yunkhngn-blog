e08606e29ba93988c3cf3c58251a34f8c969763c
yunkhngn <rogkhoa.mail@gmail.com>
2024-09-06 03:27:46 +0700

    Last commit

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import {desc} from '../lib'
 
 export default function Home({theme, themeUse}) {
   const content = {
-    Description: "Xin chào, tớ là Khoa Nguyễn. Tớ là sinh viên chuyên ngành Công nghệ thông tin tại Đại học FPT. Hiện tại tớ cũng đang là freelancer thiết kế đồ hoạ và phát triển một số dự án cá nhân liên quan tới chuyên ngành của mình.",
+    Description: "Xin chào, tớ là Khoa Nguyễn. Tớ là sinh viên chuyên ngành Công nghệ thông tin tại Đại học FPT. Hiện tại tớ cũng đang là freelancer thiết kế đồ hoạ và phát triển một số dự án cá nhân liên quan tới chuyên ngành.",
   }
   return (
     <Template description={desc.home} height="100%">
