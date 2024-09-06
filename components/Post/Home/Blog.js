850ec13862a783b0758c42c8852689a08f3aaac8
yunkhngn <rogkhoa.mail@gmail.com>
2024-09-07 01:05:24 +0700

    Chẳng biết up j

--- components/Post/Home/Blog.js
+++ components/Post/Home/Blog.js
@@ -1,20 +1,21 @@
-import { Para } from '../../Template';
-import { Div, Image } from 'atomize';
-import ElementSpace from '../ElementSpace';
+import { Para } from "../../Template";
+import { Div, Image } from "atomize";
+import ElementSpace from "../ElementSpace";
 
-const Blog = ({ theme, themeUse, data}) => {
-
+const Blog = ({ theme, themeUse, data }) => {
   const dateFormer = (date) => {
-    let dateArr = date.split('T')[0].split('-');
+    let dateArr = date.split("T")[0].split("-");
     return `${dateArr[2]}/${dateArr[1]}/${dateArr[0]}`;
   };
 
   return (
     <article>
-      <Para color={themeUse.secondary}>Các thiết kế của tớ được đăng trên Behance.</Para>
-      <Div m={{ b: '1.7em' }} />
-      <hr className={'hr' + theme} />
-      <Div p={{ b: '1em' }}>
+      <Para color={themeUse.secondary}>
+        Các thiết kế của tớ được đăng trên Behance.
+      </Para>
+      <Div m={{ b: "1.7em" }} />
+      <hr className={"hr" + theme} />
+      <Div p={{ b: "1em" }}>
         {data.length === 0 ? (
           <Para color={themeUse.secondary}>Chưa có post nào ở đây.</Para>
         ) : (
@@ -26,53 +27,53 @@ const Blog = ({ theme, themeUse, data}) => {
                   align="center"
                   d="flex"
                   flexWrap="wrap"
-                  hoverBg={theme === 'light' ? 'gray200' : '#222222'}
-                  rounded={{ xs: '26px', sm: '24px' }}
-                  p={{ xs: '16px', md: '16px' }}
+                  hoverBg={theme === "light" ? "gray200" : "#222222"}
+                  rounded={{ xs: "26px", sm: "24px" }}
+                  p={{ xs: "16px", md: "16px" }}
                   transition
-                  m={{ r: '-16px', l: '-16px' }}
+                  m={{ r: "-16px", l: "-16px" }}
                   textAlign="left"
                 >
                   <Image
                     src={item.attributes.Image}
                     alt={item.attributes.Title}
-                    h={{ xs: '70px', sm: '100px' }}
-                    w={{ xs: '70px', sm: '100px' }}
-                    rounded={{ xs: '12px', sm: '16px' }}
-                    m={{ r: '1em' }}
+                    h={{ xs: "70px", sm: "100px" }}
+                    w={{ xs: "70px", sm: "100px" }}
+                    rounded={{ xs: "12px", sm: "16px" }}
+                    m={{ r: "1em" }}
                   />
                   <Div>
                     <Para
                       margin="true"
                       which="right"
-                      color={theme === 'light' ? '#171717' : '#ededed'}
-                      d={{ xs: 'block', sm: 'flex' }}
-                      textAlign={{ xs: 'center', sm: 'left' }}
+                      color={theme === "light" ? "#171717" : "#ededed"}
+                      d={{ xs: "block", sm: "flex" }}
+                      textAlign={{ xs: "center", sm: "left" }}
                     >
                       <strong>{item.attributes.Title}</strong>
                     </Para>
                     <Para
-                      margin={{ b: '16px' }}
+                      margin={{ b: "16px" }}
                       which="right"
-                      color={theme === 'light' ? '#171717' : '#ededed'}
-                      d={{ xs: 'block', sm: 'flex' }}
-                      textAlign={{ xs: 'center', sm: 'left' }}
+                      color={theme === "light" ? "#171717" : "#ededed"}
+                      d={{ xs: "block", sm: "flex" }}
+                      textAlign={{ xs: "center", sm: "left" }}
                     >
                       {item.attributes.desc}
                     </Para>
                   </Div>
                   <hr
-                    className={'hr' + theme}
+                    className={"hr" + theme}
                     style={{
-                      margin: '16px 0',
-                      width: '100%',
+                      margin: "16px 0",
+                      width: "100%",
                     }}
                   />
                   <Para
-                    margin={{ t: '16px' }}
+                    margin={{ t: "16px" }}
                     which="left"
                     color={themeUse.secondary}
-                    textAlign={{ xs: 'center', sm: 'left' }}
+                    textAlign={{ xs: "center", sm: "left" }}
                     d={{ xs: "block", md: "flex" }}
                   >
                     {dateFormer(item.attributes.createdAt)}
@@ -83,8 +84,16 @@ const Blog = ({ theme, themeUse, data}) => {
           ))
         )}
       </Div>
-      <a target="_blank" rel="noreferrer" href="https://www.behance.net/yunkhngn_">
-        <Div textColor={themeUse.secondary} hoverTextColor={themeUse.hover} transition>
+      <a
+        target="_blank"
+        rel="noreferrer"
+        href="https://www.behance.net/yunkhngn_"
+      >
+        <Div
+          textColor={themeUse.secondary}
+          hoverTextColor={themeUse.hover}
+          transition
+        >
           Xem Behance của tớ...
         </Div>
       </a>
