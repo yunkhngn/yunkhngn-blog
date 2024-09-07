cbe97d42258cb21b8c0a447c247c16bf7293aa2c
yunkhngn <rogkhoa.mail@gmail.com>
2024-09-08 01:55:35 +0700

    Sửa form

--- components/Post/Home/ContactForm.js
+++ components/Post/Home/ContactForm.js
@@ -8,6 +8,7 @@ import HCaptcha from "@hcaptcha/react-hcaptcha";
 
 const ContactForm = ({ theme, themeUse, desc }) => {
   const onDevelopmentEnv = process.env.NODE_ENV === "development";
+  const [submited, setSubmited] = useState(false);
   const [data, setData] = React.useState({
     name: "",
     email: "",
@@ -24,27 +25,30 @@ const ContactForm = ({ theme, themeUse, desc }) => {
       authenticated
     ) {
       submitForm();
+      setSubmited(true);
     } else {
       alert("Cậu chưa điền đủ thông tin hoặc chưa xác nhận captcha");
     }
   };
-
-  const [authenticated, setAuthenticated] = useState(onDevelopmentEnv ? true : false);
-  console.log(authenticated);
+  const [authenticated, setAuthenticated] = useState(
+    onDevelopmentEnv ? true : false
+  );
   const submitForm = (event) => {
     window.scrollTo({ top: 0, behavior: "smooth" });
     if (!onDevelopmentEnv) {
-      emailjs.send(process.env.SERVICE_ID,process.env.TEMPLATE_ID,{
-        email: data.email,
-        from_name: data.name,
-        message: data.message,
-        title: data.title,
+      emailjs.send(
+        process.env.SERVICE_ID,
+        process.env.TEMPLATE_ID,
+        {
+          email: data.email,
+          from_name: data.name,
+          message: data.message,
+          title: data.title,
         },
         process.env.PUBLIC_KEY
       );
       alert("Cảm ơn cậu đã liên hệ với tớ, tớ sẽ trả lời cậu sớm nhất có thể");
-    }
-    else {
+    } else {
       alert("Đã gửi r nhé");
     }
   };
@@ -63,15 +67,16 @@ const ContactForm = ({ theme, themeUse, desc }) => {
         >
           Họ và tên
         </Text>
-        <Input 
+        <Input
           m={{ b: "1em" }}
-          h="3.5em" 
+          h="3.5em"
           bg={theme === "light" ? "#f9f9f9" : "rgba(20,20,20)"}
           borderColor={theme === "light" ? "#f9f9f9" : "#171717"}
           focusBorderColor="none"
           textColor={theme === "light" ? "#333" : "#858585"}
           onChange={(e) => setData({ ...data, name: e.target.value })}
-          />
+          placeholder="Họ và tên của bạn"
+        />
         <Text
           tag="section"
           textSize="title"
@@ -82,17 +87,15 @@ const ContactForm = ({ theme, themeUse, desc }) => {
           Email
         </Text>
         <Input
-        m={{ b: "1em" }}
-        h="3.5em" 
-        bg={theme === "light" ? "#f9f9f9" : "rgba(20,20,20)"}
-        borderColor={theme === "light" ? "#f9f9f9" : "#171717"}
-        focusBorderColor="none"
-        textColor={theme === "light" ? "#333" : "#858585"}
-        onChange={(e) => setData({ ...data, email: e.target.value })}
+          m={{ b: "1em" }}
+          h="3.5em"
+          bg={theme === "light" ? "#f9f9f9" : "rgba(20,20,20)"}
+          borderColor={theme === "light" ? "#f9f9f9" : "#171717"}
+          focusBorderColor="none"
+          textColor={theme === "light" ? "#333" : "#858585"}
+          onChange={(e) => setData({ ...data, email: e.target.value })}
+          placeholder="example@abc.com"
         />
-        <Text tag="section" textSize="paragraph" m={{ b: "0.5em" }}>
-          example@abc.com
-        </Text>
         <Text
           tag="section"
           textSize="title"
@@ -103,13 +106,14 @@ const ContactForm = ({ theme, themeUse, desc }) => {
           Tiêu đề
         </Text>
         <Input
-        m={{ b: "1em" }}
-        h="3.5em" 
-        bg={theme === "light" ? "#f9f9f9" : "rgba(20,20,20)"}
-        borderColor={theme === "light" ? "#f9f9f9" : "#171717"}
-        focusBorderColor="none"
-        textColor={theme === "light" ? "#333" : "#858585"}
-        onChange={(e) => setData({ ...data, title: e.target.value })}
+          m={{ b: "1em" }}
+          h="3.5em"
+          bg={theme === "light" ? "#f9f9f9" : "rgba(20,20,20)"}
+          borderColor={theme === "light" ? "#f9f9f9" : "#171717"}
+          focusBorderColor="none"
+          textColor={theme === "light" ? "#333" : "#858585"}
+          onChange={(e) => setData({ ...data, title: e.target.value })}
+          placeholder="Tiêu đề"
         />
         <Text
           tag="section"
@@ -121,36 +125,41 @@ const ContactForm = ({ theme, themeUse, desc }) => {
           Nội dung
         </Text>
         <Textarea
-        m={{ b: "1em" }}
-        h="250px" 
-        bg={theme === "light" ? "#f9f9f9" : "rgba(20,20,20)"}
-        borderColor={theme === "light" ? "#f9f9f9" : "#171717"}
-        focusBorderColor="none"
-        textColor={theme === "light" ? "#333" : "#858585"}
-        onChange={(e) => setData({ ...data, message: e.target.value })}
+          m={{ b: "1em" }}
+          h="250px"
+          bg={theme === "light" ? "#f9f9f9" : "rgba(20,20,20)"}
+          borderColor={theme === "light" ? "#f9f9f9" : "#171717"}
+          focusBorderColor="none"
+          textColor={theme === "light" ? "#333" : "#858585"}
+          onChange={(e) => setData({ ...data, message: e.target.value })}
+          placeholder="Nội dung"
+          fontFamily="primary"
         />
-        {!onDevelopmentEnv && <HCaptcha
-                sitekey="31677f64-0983-4d5f-afcf-bcb06e4a6bc6"
-                onVerify={(token, ekey) => {
-                  setAuthenticated(true);
-                }}
-                theme={theme === "light" ? "light" : "dark"}
-              ></HCaptcha>}
+        {onDevelopmentEnv && (
+          <Div m={{ t: "1em" }}>
+            <Text tag="section" textSize="paragraph" m={{ b: "0.5em" }}>
+              Lưu ý: điền captcha trước khi submit nhé!
+            </Text>
+            <HCaptcha
+            sitekey="31677f64-0983-4d5f-afcf-bcb06e4a6bc6"
+            onVerify={() => {
+              setAuthenticated(true);
+            }}
+            theme={theme === "light" ? "light" : "dark"}
+            ></HCaptcha>
+        </Div>
+        )}
         <Button
-        textColor={theme === "light" ? "#ededed" : "#171717"}
-        bg={theme === "light" ? "#171717" : "#ededed"}
-        hoverBg={theme === "light" ? "black700" : "gray500"}
-        w="100%"
-        onClick={handleSubmit}
-        suffix={
-          <Icon
-            name="RightArrow"
-            size="16px"
-            color={theme === "light" ? "#ededed" : "#171717"}
-            m={{ l: "0.5rem" }}
-          />
-        }
-        >Gửi cho tớ</Button>
+          textColor={theme === "light" ? "#ededed" : "#171717"}
+          bg={theme === "light" ? "#171717" : "#ededed"}
+          hoverBg={theme === "light" ? "black700" : "gray500"}
+          w="100%"
+          onClick={handleSubmit}
+          disabled={submited}
+          m={{ t: "1em" }}
+        >
+          Gửi cho tớ
+        </Button>
       </Div>
       <ElementSpace space="12em" />
     </article>
