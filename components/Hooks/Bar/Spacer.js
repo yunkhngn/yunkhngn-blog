c5e548d56da90120ce143474def8d82a47a70287
yunkhngn <rogkhoa.mail@gmail.com>
2024-09-07 20:35:36 +0700

    Fix spacer in bar

--- components/Hooks/Bar/Spacer.js
+++ components/Hooks/Bar/Spacer.js
@@ -1,7 +1,7 @@
 import {Div} from 'atomize'
 const Spacer = ({theme}) => {
     return (
-        <Div h="40px" border={{r:"1.9px solid"}} rounded="xs" m="5px" borderColor={theme === 'light' ? "hsl(0 0% 0% / 0.071)" : "hsl(0 0% 100% / 0.077)"}/>
+        <Div h="40px" border={{r:"1.9px solid"}} rounded="xs" m="7px" borderColor={theme === 'light' ? "hsl(0 0% 0% / 0.071)" : "hsl(0 0% 100% / 0.077)"}/>
     );
 }
 export default Spacer;
