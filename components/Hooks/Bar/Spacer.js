acfda60e208b3b1c5f5e9ed26a6ffda69fbe5f25
yunkhngn <rogkhoa.mail@gmail.com>
2024-09-13 02:39:10 +0700

    update animation

--- components/Hooks/Bar/Spacer.js
+++ components/Hooks/Bar/Spacer.js
@@ -1,7 +1,15 @@
-import {Div} from 'atomize'
-const Spacer = ({theme}) => {
-    return (
-        <Div h="40px" border={{r:"1.9px solid"}} rounded="xs" m="7px" borderColor={theme === 'light' ? "hsl(0 0% 0% / 0.071)" : "hsl(0 0% 100% / 0.077)"}/>
-    );
-}
+import { Div } from "atomize";
+const Spacer = ({ theme }) => {
+  return (
+    <Div
+      h="42px"
+      border={{ r: "1.9px solid" }}
+      rounded="xs"
+      m="5px"
+      borderColor={
+        theme === "light" ? "hsl(0 0% 0% / 0.071)" : "hsl(0 0% 100% / 0.077)"
+      }
+    />
+  );
+};
 export default Spacer;
