2c0a5e2df99878278288f7756e280842cb921fff
yunkhngn <rogkhoa.mail@gmail.com>
2024-09-14 15:56:45 +0700

    Padding size of writing

--- components/Post/Body/Write.js
+++ components/Post/Body/Write.js
@@ -1,33 +1,32 @@
-import { Para,ElementSpace, Spacer } from '../../Template';
-import { Div, Input } from 'atomize';
-import Link from 'next/link';
+import { Para, ElementSpace, Spacer } from "../../Template";
+import { Div, Input } from "atomize";
+import Link from "next/link";
 
 const formatDate = (dateString) => {
   const date = new Date(dateString);
-  const day = String(date.getDate()).padStart(2, '0'); // Đảm bảo có 2 chữ số cho ngày
-  const month = String(date.getMonth() + 1).padStart(2, '0'); // Tháng bắt đầu từ 0, cộng thêm 1
+  const day = String(date.getDate()).padStart(2, "0"); // Đảm bảo có 2 chữ số cho ngày
+  const month = String(date.getMonth() + 1).padStart(2, "0"); // Tháng bắt đầu từ 0, cộng thêm 1
   const year = date.getFullYear();
   return `${day}/${month}/${year}`;
 };
 
 const searchPost = (e) => {
-  const posts = document.querySelectorAll('.post');
+  const posts = document.querySelectorAll(".post");
   posts.forEach((post) => {
     if (post.innerText.toLowerCase().includes(e.target.value.toLowerCase())) {
-      post.style.display = 'block';
+      post.style.display = "block";
     } else {
-      post.style.display = 'none';
+      post.style.display = "none";
     }
-  }
-  );
+  });
 };
 
-const Write = ({desc, theme, themeUse, content }) => {
+const Write = ({ desc, theme, themeUse, content }) => {
   return (
     <article>
       <Para color={themeUse.secondary}>{desc.desc}</Para>
-      <Div m={{ b: '1.7em' }} />
-      <hr className={'hr' + theme} />
+      <Div m={{ b: "1.7em" }} />
+      <hr className={"hr" + theme} />
       <Para textSize="subheader" color={themeUse.primary}>
         Tìm kiếm các bài viết của tớ
       </Para>
@@ -47,8 +46,8 @@ const Write = ({desc, theme, themeUse, content }) => {
         border="1px solid"
         bg={theme === "light" ? "#f9f9f9" : "rgba(20,20,20)"}
         borderColor={theme === "light" ? "gray300" : "#171717"}
-        />
-        <Spacer length="100%" theme={theme} />
+      />
+      <Spacer length="100%" theme={theme} />
       <Div>
         {content.length === 0 ? (
           <Para color={themeUse.secondary}>Chưa có post nào ở đây.</Para>
@@ -60,20 +59,42 @@ const Write = ({desc, theme, themeUse, content }) => {
                   justify="flex-start"
                   align="center"
                   d="flex"
-                  hoverBg={theme === 'light' ? 'gray200' : '#222222'}
+                  hoverBg={theme === "light" ? "gray200" : "#222222"}
                   rounded="12px"
-                  p="16px"
+                  p={{xs : "10px", md: "16px"}}
                   transition
-                  m={{ r: '-16px', l: '-16px' }}
+                  m={{xs : "0 -10px 0 -10px", md: "0 -16px 0 -16px"}}
                 >
-                  <Para margin="true" which="right" color={theme === 'light' ? '#171717' : '#ededed'}>
+                  <Para
+                    margin="true"
+                    which="right"
+                    color={theme === "light" ? "#171717" : "#ededed"}
+                    p={{xs : "10px", md: "16px"}}
+                  transition
+                  m={{xs : "0 -10px 0 -10px", md: "0 -16px 0 -16px"}}
+                  >
                     <strong>{item.attributes.Title}</strong>
                   </Para>
-                  <Para w={{ xs: '170px', md: '350px' }} margin="true" which="right" color={theme === 'light' ? '#171717' : '#ededed'}>
+                  <Para
+                    w={{ xs: "170px", md: "350px" }}
+                    margin="true"
+                    which="right"
+                    color={theme === "light" ? "#171717" : "#ededed"}
+                    p={{xs : "10px", md: "16px"}}
+                  transition
+                  m={{xs : "0 -10px 0 -10px", md: "0 -16px 0 -16px"}}
+                  >
                     {item.attributes.Desc}
                   </Para>
-                  <hr className={'hr' + theme} />
-                  <Para margin="true" which="left" color={themeUse.secondary}>
+                  <hr className={"hr" + theme} />
+                  <Para 
+                  margin="true" 
+                  which="left" 
+                  color={themeUse.secondary} 
+                  p={{xs : "10px", md: "16px"}}
+                  transition
+                  m={{xs : "0 -10px 0 -10px", md: "0 -16px 0 -16px"}}
+                  >
                     {formatDate(item.attributes.createdAt)}
                   </Para>
                 </Div>
