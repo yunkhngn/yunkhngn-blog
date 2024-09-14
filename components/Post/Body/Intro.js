36f0a6aa6407069a6880b3dd20e4215e42900b45
yunkhngn <rogkhoa.mail@gmail.com>
2024-09-14 14:51:02 +0700

    Adding change log to intro

--- components/Post/Body/Intro.js
+++ components/Post/Body/Intro.js
@@ -1,5 +1,7 @@
 import {Para, SocialLink, Spacer} from '../../Template'
 import ReactMarkdown from 'react-markdown'
+import Link from 'next/link'
+import {Div} from 'atomize'
 
 const Intro = ({theme,themeUse, content}) => {
     return (
@@ -8,11 +10,20 @@ const Intro = ({theme,themeUse, content}) => {
             <Spacer theme={theme} length={{xs:"100%", md:"200px"}} />
             <ReactMarkdown>{content}</ReactMarkdown>
             <br/>
-
             <i><span className={"keyboard kb--"+theme}>&#8984;</span><span className={"keyboard kb--"+theme}>k</span>để khám phá...</i>
             <Spacer theme={theme} length="120px"/>
             <SocialLink theme={theme}/>
-
+            <Spacer theme={theme} length="60px"/>
+            <Link href="/dev/change-log" passHref scroll={true}>
+            <Div
+                m={{ t: "1em" }}
+                textColor={themeUse.secondary}
+                hoverTextColor={themeUse.hover}
+                transition
+                >
+                Change log...
+            </Div>
+            </Link>
         </article>
     );
 }
