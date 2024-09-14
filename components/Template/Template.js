03118ba39d016ad3dd7f93920fbb7ba655372c9e
yunkhngn <rogkhoa.mail@gmail.com>
2024-09-14 16:13:11 +0700

    Change size of template top

--- components/Template/Template.js
+++ components/Template/Template.js
@@ -1,13 +1,20 @@
-import {Div} from 'atomize'
-import Metatags from './Metatags'
+import { Div } from "atomize";
+import Metatags from "./Metatags";
 
-const Template = ({children, description, height}) => {
-    return (
-        <Div maxW="720px" m="auto" justify="center" align="center" p={{t:{xs:"50px", md:"120px"}, l:"16px", r:"16px"}} h={height}>
-            <Metatags description={description}/>
-            {children}
-        </Div>
-    );
-}
+const Template = ({ children, description, height }) => {
+  return (
+    <Div
+      maxW="720px"
+      m="auto"
+      justify="center"
+      align="center"
+      p={{ t: { xs: "70px", md: "100px" }, l: "16px", r: "16px" }}
+      h={height}
+    >
+      <Metatags description={description} />
+      {children}
+    </Div>
+  );
+};
 
 export default Template;
