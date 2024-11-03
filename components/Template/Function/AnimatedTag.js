b7d18ca5de180fdebd278f62a04e9ed1d0c485e7
yunkhngn <rogkhoa.mail@gmail.com>
2024-11-04 03:19:28 +0700

    Update animation

--- components/Template/Function/AnimatedTag.js
+++ components/Template/Function/AnimatedTag.js
@@ -3,15 +3,12 @@ import { motion} from 'framer-motion'
 const animation = {
     initial: {
       opacity: 0,
-      y : -50,
     },
     animate: {
       opacity: 1,
-      y : 0,
     },
     exit: {
       opacity: 0,
-      y : -50,
     }
   }
 
