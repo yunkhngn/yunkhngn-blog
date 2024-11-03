f9b46dd1bef83d2056f2b6580c7a07c083abd35c
yunkhngn <rogkhoa.mail@gmail.com>
2024-11-04 03:54:29 +0700

    Test animation

--- components/Template/Function/AnimatedTag.js
+++ components/Template/Function/AnimatedTag.js
@@ -3,15 +3,12 @@ import { motion} from 'framer-motion'
 const animation = {
     initial: {
       opacity: 0,
-      y: 30,
     },
     animate: {
       opacity: 1,
-      y: 0,
     },
     exit: {
       opacity: 0,
-      y: -20,
     }
   }
 
