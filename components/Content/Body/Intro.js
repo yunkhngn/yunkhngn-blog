18585c69b2f4e62abadfaff8e818764c64e51666
yunkhngn <rogkhoa.mail@gmail.com>
2024-12-06 16:11:13 +0700

    Pro Tip: Read Copilot output before pushing it

--- components/Content/Body/Intro.js
+++ components/Content/Body/Intro.js
@@ -7,7 +7,7 @@ const Intro = ({theme,themeUse, content}) => {
     return (
         <article>
             <Para color={themeUse.secondary}>Graphic Designer / Software Engineer</Para>
-            <Spacer theme={theme} length={{xs:"100%", md:"200px"}} />
+            <Spacer theme={theme} length={{xs:"100%", md:"200px"}}/>
             <ReactMarkdown>{content}</ReactMarkdown>
             <br/>
             <i><span className={"keyboard kb--"+theme}>&#8984;</span><span className={"keyboard kb--"+theme}>k</span>để khám phá...</i>
