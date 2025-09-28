a980be84de86cd7f25e00e0744a04c81eb909937
yunkhngn <rogkhoa.mail@gmail.com>
2025-09-28 14:08:45 +0700

    debug step 2: add router and basic state management

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-// Debugging step 1: Add basic styling
+// Debugging step 2: Add router and basic state
 import "../styles/globals.css";
 import "../styles/Bar.css";
 import "../styles/BarIcon.css";
@@ -6,12 +6,23 @@ import "../styles/BarIcon.css";
 import { styletron } from "../styletron";
 import { Provider as StyletronProvider } from "styletron-react";
 import { StyleReset } from "atomize";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import { themeProvider } from "../components/lib";
 
 function MyApp({ Component, pageProps }) {
+  const [theme, setTheme] = useState("");
+  const [themeUse, setThemeUse] = useState(themeProvider[0]);
+  const router = useRouter();
+
   return (
     <StyletronProvider value={styletron}>
       <StyleReset />
-      <Component {...pageProps} />
+      <Component 
+        themeUse={themeUse.styles}
+        theme={theme}
+        {...pageProps} 
+      />
     </StyletronProvider>
   );
 }
