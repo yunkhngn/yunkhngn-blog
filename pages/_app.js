47830c900a43a3fd50ea72ecb5d635c7ce667638
yunkhngn <rogkhoa.mail@gmail.com>
2025-09-28 14:09:13 +0700

    debug step 3: add NProgress (potential issue)

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
-// Debugging step 2: Add router and basic state
+// Debugging step 3: Add NProgress (potential culprit)
 import "../styles/globals.css";
 import "../styles/Bar.css";
 import "../styles/BarIcon.css";
+
+import 'nprogress/nprogress.css';
+import NProgress from 'nprogress';
 
 import { styletron } from "../styletron";
 import { Provider as StyletronProvider } from "styletron-react";
@@ -14,6 +17,28 @@ function MyApp({ Component, pageProps }) {
   const [theme, setTheme] = useState("");
   const [themeUse, setThemeUse] = useState(themeProvider[0]);
   const router = useRouter();
+
+  useEffect(() => {
+    NProgress.configure({ showSpinner: false, trickleSpeed: 120 });
+    let timer;
+    const start = () => {
+      clearTimeout(timer);
+      timer = setTimeout(() => NProgress.start(), 120);
+    };
+    const done = () => {
+      clearTimeout(timer);
+      NProgress.done();
+    };
+
+    router.events.on('routeChangeStart', start);
+    router.events.on('routeChangeComplete', done);
+    router.events.on('routeChangeError', done);
+    return () => {
+      router.events.off('routeChangeStart', start);
+      router.events.off('routeChangeComplete', done);
+      router.events.off('routeChangeError', done);
+    };
+  }, [router.events]);
 
   return (
     <StyletronProvider value={styletron}>
