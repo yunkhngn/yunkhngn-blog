681da876a78b43139ad8626a991128d98683d876
yunkhngn <rogkhoa.mail@gmail.com>
2025-09-28 14:08:16 +0700

    debug step 1: add styletron and basic styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,19 @@
-// Minimal _app.js for debugging
+// Debugging step 1: Add basic styling
 import "../styles/globals.css";
+import "../styles/Bar.css";
+import "../styles/BarIcon.css";
+
+import { styletron } from "../styletron";
+import { Provider as StyletronProvider } from "styletron-react";
+import { StyleReset } from "atomize";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <StyletronProvider value={styletron}>
+      <StyleReset />
+      <Component {...pageProps} />
+    </StyletronProvider>
+  );
 }
 
 export default MyApp;
