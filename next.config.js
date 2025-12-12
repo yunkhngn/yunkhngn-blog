db2b101650b180438dcbf148c7642195cd8920c6
yunkhngn <rogkhoa.mail@gmail.com>
2025-12-12 14:48:00 +0700

    fixfix: react2shell

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  turbopack: {},
   i18n: {
     locales: ["vi"],
     defaultLocale: "vi",
--- pages/_app_minimal.js
+++
@@ -1,8 +0,0 @@
-// Minimal _app.js for debugging
-import "../styles/globals.css";
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
